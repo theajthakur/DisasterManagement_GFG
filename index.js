34552352cfc2f76d9ee3faa34712727291102b71
Vijay Singh <thakurvijayofficial@gmail.com>
2024-09-24 00:36:01 +0530

    Modified Middleware to Validate Token and Handle them accordingly

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
+const authMiddleware = require("./middlewares/authMiddleware");
 require("dotenv").config();
+const authRouter = require("./routes/auth");
 const connectDB = require("./config/db");
 connectDB();
-
-const authRouter = require("./routes/auth");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 app.use("/auth/", authRouter);
+app.use("/web/", authMiddleware);
 
 app.listen(port, () => {
   console.log(`Server Started on PORT ${port}`);
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,12 +5,12 @@ const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1]; // Get the token from the Authorization header
 
   if (!token) {
-    return res.redirect("/auth/login"); // Redirect to login if no token is found
+    return res.json({ status: "error", message: "No Token found!" });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      return res.redirect("/auth/login"); // Redirect to login if token is invalid
+      return res.json({ status: "error", message: "Invalid Token!" }); //if token is invalid
     }
 
     req.user = decoded; // Store the decoded token data in the request object
