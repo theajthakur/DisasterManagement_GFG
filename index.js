c2b69d852c8ee0f0b4e52b246bc0290d9170660d
Vijay Singh <thakurvijayofficial@gmail.com>
2024-09-24 22:48:04 +0530

    Added Css and JS in Public DIRs

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ const authRouter = require("./routes/auth");
 const connectDB = require("./config/db");
 connectDB();
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.set("view engine", "ejs");
+
 app.use("/auth/", authRouter);
 app.use("/web/", authMiddleware);
 
