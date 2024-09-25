846c01a098145f5b24629339dbeb629e9cce6e0b
Vijay Singh <thakurvijayofficial@gmail.com>
2024-09-26 00:58:06 +0530

    Upload Feature for logged In only

--- routes/static.js
+++ routes/static.js
@@ -2,11 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  if (req.user) return res.render("weather", { login: true });
-  res.render("weather");
+  const user = req.user;
+  console.log(user);
+  if (user) return res.render("weather", { login: true, user });
+  return res.render("weather");
 });
 
 router.get("/feeds", async (req, res) => {
+  const user = req.user;
+  if (user) return res.render("feeds", { login: true, user });
   res.render("feeds");
 });
 
